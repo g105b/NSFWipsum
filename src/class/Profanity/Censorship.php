e7866e60594c3d60c42bfa1b03cf1b57c1bbe4f4
Greg Bowler <greg.bowler@g105b.com>
2016-11-22 19:38:28 +0000

    Censor initial profanity list.

--- src/class/Profanity/Censorship.php
+++ src/class/Profanity/Censorship.php
@@ -1,13 +1,33 @@
 <?php
 namespace NSFWipsum\Profanity;
 
+use Gt\Core\Path;
+
 class Censorship {
 
+private static $profanityList;
+
 public static function isProfane(string $word):bool {
-	if($word === "two") {
-		return true;
+	$profane = false;
+
+	foreach(self::profanityListSingleton() as $profanity) {
+		if(strstr($word, $profanity)) {
+			$profane = true;
+		}
 	}
-	return false;
+
+	return $profane;
+}
+
+private static function profanityListSingleton() {
+	if(empty(self::$profanityList)) {
+		self::$profanityList = explode(
+			"\n",
+			file_get_contents(Path::get(Path::ROOT) . "/profanity-list"
+		));
+	}
+
+	return self::$profanityList;
 }
 
 }#
